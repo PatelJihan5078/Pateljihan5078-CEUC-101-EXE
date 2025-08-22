0aa69d9a3ab2481eef65edd4e68dabd455b3d757
PatelJihan5078 <jihan7850785@gmail.com>
2025-08-22 16:08:37 +0530

    Add files via upload

--- P16.C
+++ P16.C
@@ -1,54 +1,31 @@
-/* Program : GAME ON.
-   Programmer : Patel Jihan
-   Date : 18/08/2025
+/*  Program : Game.
+    Programer : Jihan Patel
+    Date : 22/07/25
 */
-#include <stdio.h>
-#include <stdlib.h>
-#include <dos.h>   // For delay() in Turbo C
-
-int main() {
-    int matchsticks = 21;
-    int player, computer;
-
-    printf("Welcome to the 21 Matchstick Game!\n");
-    printf("Rules:\n- There are 21 matchsticks.\n- You can pick 1 to 4 matchsticks on your turn.\n");
-    printf("- Whoever picks the last matchstick loses.\n\n");
-
-    while (matchsticks > 1) {
-        printf("Matchsticks remaining: %d\n", matchsticks);
-        printf("Your turn (pick 1 to 4 matchsticks): ");
-        scanf("%d", &player);
-
-        // Validate input
-        if (player < 1 || player > 4) {
-            printf("Invalid move! You must pick between 1 and 4 matchsticks.\n");
-            continue;
-        }
-
-        matchsticks -= player;
-
-        if (matchsticks <= 1) {
-            printf("Only %d matchstick left.\n", matchsticks);
-            printf("You picked the last matchstick. You lose!\n");
-            break;
-        }
-
-        // Computer's move: make total moves = 5
-        computer = 5 - player;
-        printf("Computer picks %d matchstick(s).\n", computer);
-        delay(1000); // Wait 1 second for effect
-
-        matchsticks -= computer;
-
-        if (matchsticks <= 1) {
-            printf("Only %d matchstick left.\n", matchsticks);
-            printf("Computer picked the last matchstick. You win!\n");
-            break;
-        }
-
-        printf("\n");
-    }
-
-    printf("Game Over.\n");
-    return 0;
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n=21,ch;
+	clrscr();
+	printf("You must be choose 1,2,3 & 4 matchstick.\n");
+	printf("Your game starts now.\n");
+	printf("How many matchstick you want to choose?\n");
+	while(n<22)
+	{
+		if(n==1)
+		{
+			printf("You looses the game.");
+			break;
+		}
+		else
+		{
+			printf("\n* Your turn:");
+			scanf("%d",&ch);
+			printf("\n* Computers turn:%d",(5-ch));
+			n-=5;
+			printf("\t %d matchstick remains.\n",(n));
+		}
+	}
+	getch();
 }
