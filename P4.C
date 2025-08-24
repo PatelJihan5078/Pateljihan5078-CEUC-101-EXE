22f33678842afb0ec2fc4b2c4840533980f044a9
PatelJihan5078 <jihan7850785@gmail.com>
2025-08-24 21:32:14 +0530

    Add files via upload

---
+++ P4.C
@@ -0,0 +1,20 @@
+/*
+  Aim:To find the size of database
+  programmer: Patel Jihan
+  Date: 21/07/2025  
+  */
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char c;
+	int i;s
+	float f;
+	double d;
+	clrscr();
+	printf("char size is %d", sizeof(c));
+	printf("\nint size of %d", sizeof(i));
+	printf("\nfloat size of %d", sizeof(f));
+	printf("\ndouble size of %d", sizeof(d));
+	getch();
+}
