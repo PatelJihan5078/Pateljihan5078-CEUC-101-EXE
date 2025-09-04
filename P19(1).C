1830bd8cf9e3a2fba613ee0b496fcd25acfaa5fd
PatelJihan5078 <jihan7850785@gmail.com>
2025-09-04 14:51:35 +0530

    Add files via upload

---
+++ P19(1).C
@@ -0,0 +1,25 @@
+/* Aim: Create a Pattern
+   Programmer: Patel Jihan
+   Date: 09/04/25
+*/
+#include<stdio.h>
+
+void main()
+{
+	int i, j;
+	clrscr();
+	for (i=1; i<=5; i++)
+	{
+		for (j=1; j<=i;j++)
+		{
+			if(j%2 == 1){
+			printf("1  ");
+			}else{
+			printf("0  ");
+		       }
+		}
+		printf("\n");
+
+	 }
+	getch();
+}
