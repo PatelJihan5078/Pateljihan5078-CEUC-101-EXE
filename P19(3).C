c893b9f53b28ef1f30a6daeafca73d4fc3674572
PatelJihan5078 <jihan7850785@gmail.com>
2025-09-09 09:56:02 +0530

    Add files via upload

--- P19(3).C
+++ P19(3).C
@@ -16,7 +16,18 @@ void main()
 		for(k=2; k<=i; k++)  {
 			printf("%d",k); }
 		printf("\n");
+
 	}
+	for(i=2; i<=n; i++){
+		for(s=1; s<=n-i; s++){
+		printf(" "); }
+		for(j=n-i+1; j<=n;j++) {
+		printf("%d",n-j+1);  }
+		for(k=2;k<=i;k++){
+		printf("%d",k);  }
+
+		printf("\n");
+		}
 	getch();
 }
 
