43e7a8d6ec9b34afd5ff6951d28ddf3d7bb7eaaf
PatelJihan5078 <jihan7850785@gmail.com>
2025-09-16 09:40:32 +0530

    Add files via upload

---
+++ P19(4).C
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    int C,R,n,Ch;
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+    for(R=1;R<=n;R++)
+    {
+        Ch=64;
+        for(C=1;C<=n-R;C++)
+        {
+            printf(" ");
+        }
+        for(C=n-R+1;C<=n;C++)
+        {
+            Ch++;
+            printf("%c",Ch);
+        }
+        for(C=n+1;C<=n+R-1;C++)
+        {
+            Ch--;
+            printf("%c",Ch);
+        }
+        printf("\n");
+    }
+    for(R=2;R<=n;R++)
+    {
+        Ch=64;
+        for(C=1;C<=R-1;C++)
+        {
+            printf(" ");
+        }
+        for(C=R;C<=n;C++)
+        {
+            Ch++;
+            printf("%c",Ch);
+        }
+        for(C=n+1;C<=2*n-R;C++)
+        {
+            Ch--;
+            printf("%c",Ch);
+        }
+        printf("\n");
+    }
+}
