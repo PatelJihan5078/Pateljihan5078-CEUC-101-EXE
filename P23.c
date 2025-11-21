039a1572900b6cd8983f5c94ad6e08e2c7f4d9e0
PatelJihan5078 <jihan7850785@gmail.com>
2025-11-21 11:11:02 +0530

    Add files via upload

--- P23.c
+++ P23.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
-void fun(int x)
-{
-x = 30;
-}
-int main(void)
-{
 
-int x = 20;
-fun(x);
-printf("x = %d", x);
-return 0;
+int main() {
+    int n, i;
+    printf("Jihan Patel\n25CE079\n\n");
+    printf("Enter number of days: ");
+    scanf("%d", &n);
+
+    int prices[n];
+    printf("Enter fruit prices for each day:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &prices[i]);
+    }
+
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for(i = 1; i < n; i++) {
+        if(prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+        else {
+            int profit = prices[i] - minPrice;
+            if(profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+
+    printf("Maximum Profit: %d\n", maxProfit);
+
+    return 0;
 }
